f31cef3ca378a4a1914c2b6cb99a223b5e579105
pvy <pvy@odoo.com>
2015-11-05 16:51:21 +0100

    [REF] equipment: changed equipment so it doesn't depend on hr anymore

--- addons/equipment/models/res_config.py
+++ addons/equipment/models/res_config.py
@@ -4,8 +4,8 @@ from urlparse import urlsplit
 from openerp import api, fields, models
 
 
-class HrEquipmentConfigSettings(models.TransientModel):
-    _name = 'hr.equipment.config.settings'
+class EquipmentConfigSettings(models.TransientModel):
+    _name = 'equipment.config.settings'
     _inherit = 'res.config.settings'
 
     equipment_alias_prefix = fields.Char('Use the following alias to report internal equipment issue')
@@ -14,7 +14,7 @@ class HrEquipmentConfigSettings(models.TransientModel):
     @api.multi
     def get_default_alias_equipment(self):
         alias_name = False
-        alias_id = self.env.ref('hr_equipment.mail_alias_equipment')
+        alias_id = self.env.ref('equipment.mail_alias_equipment')
         if alias_id:
             alias_name = alias_id.alias_name
         return {'equipment_alias_prefix': alias_name}
@@ -24,7 +24,7 @@ class HrEquipmentConfigSettings(models.TransientModel):
         for record in self:
             default_equipment_alias_prefix = record.get_default_alias_equipment()['equipment_alias_prefix']
             if record.equipment_alias_prefix != default_equipment_alias_prefix:
-                alias_id = self.env.ref('hr_equipment.mail_alias_equipment')
+                alias_id = self.env.ref('equipment.mail_alias_equipment')
                 if alias_id:
                     alias_id.write({'alias_name': record.equipment_alias_prefix})
         return True
@@ -39,4 +39,3 @@ class HrEquipmentConfigSettings(models.TransientModel):
             except Exception:
                 pass
         return {'alias_domain': alias_domain}
-
