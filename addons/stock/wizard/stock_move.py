fa22c9c1da5ef36a61c96161e6216db119224570
Josse Colpaert <jco@odoo.com>
2015-12-03 15:32:01 +0100

    [IMP] Scrap with wizard in picking and production order [IMP] Rename stock picking scrap to move scrap2

--- addons/stock/wizard/stock_move.py
+++ addons/stock/wizard/stock_move.py
@@ -1,82 +1,65 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
-import openerp.addons.decimal_precision as dp
+from openerp import api, fields, models, _
+from openerp.exceptions import UserError
 
+class StockPickingScrap(models.TransientModel):
+    _name = 'stock.scrap'
 
+    picking_id = fields.Many2one('stock.picking', 'Picking', default=(lambda x: (x.env.context.get('active_id'))), readonly=True) #x.env.context.get('active_model') == 'mrp.production.workcenter.line') and 
+    location_id = fields.Many2one('stock.location', 'Source Location')
+    scrap_location_id = fields.Many2one('stock.location', domain="[('scrap_location', '=', True)]", 
+                                        default=(lambda x: x.env['stock.location'].search([('scrap_location', '=', True)], limit=1)))
+    type = fields.Selection([('reserved', 'Reserved or Done'), ('demand', 'Useful for Picking'), ('location', 'Everything in Source Location')], default='reserved')
+    line_ids = fields.One2many('stock.scrap.line', 'scrap_id')
 
-class stock_move_scrap(osv.osv_memory):
-    _name = "stock.move.scrap"
-    _description = "Scrap Products"
-
-    _columns = {
-        'product_id': fields.many2one('product.product', 'Product', required=True, select=True),
-        'product_qty': fields.float('Quantity', digits_compute=dp.get_precision('Product Unit of Measure'), required=True),
-        'product_uom': fields.many2one('product.uom', 'Product Unit of Measure', required=True),
-        'location_id': fields.many2one('stock.location', 'Location', required=True),
-        'restrict_lot_id': fields.many2one('stock.production.lot', 'Lot'),
-    }
-
-    _defaults = {
-        'location_id': lambda *x: False
-    }
-
-    def default_get(self, cr, uid, fields, context=None):
-        """ Get default values
-        @param self: The object pointer.
-        @param cr: A database cursor
-        @param uid: ID of the user currently logged in
-        @param fields: List of fields for default value
-        @param context: A standard dictionary
-        @return: default values of fields
-        """
-        if context is None:
-            context = {}
-        res = super(stock_move_scrap, self).default_get(cr, uid, fields, context=context)
-        move = self.pool.get('stock.move').browse(cr, uid, context['active_id'], context=context)
-
-        location_obj = self.pool.get('stock.location')
-        scrap_location_id = location_obj.search(cr, uid, [('scrap_location','=',True)])
-
-        if 'product_id' in fields:
-            res.update({'product_id': move.product_id.id})
-        if 'product_uom' in fields:
-            res.update({'product_uom': move.product_uom.id})
-        if 'location_id' in fields:
-            if scrap_location_id:
-                res.update({'location_id': scrap_location_id[0]})
+    def _translate_quants_to_lines(self, quants):
+        grouped_quants = {}
+        for quant in quants:
+            key = (quant.location_id.id, quant.product_id.id, quant.package_id.id, quant.lot_id.id, quant.owner_id.id)
+            if grouped_quants.get(key):
+                grouped_quants[key] += quant.qty
             else:
-                res.update({'location_id': False})
-        return res
+                grouped_quants[key] = quant.qty
+        scrap_lines = []
+        for key in grouped_quants:
+            scrap_lines += [(0, 0, {'location_id': key[0],
+                                    'product_id': key[1],
+                                    'package_id': key[2],
+                                    'lot_id': key[3],
+                                    'owner_id': key[4],
+                                    'product_qty': grouped_quants[key]})]
+        return scrap_lines
 
-    def move_scrap(self, cr, uid, ids, context=None):
-        """ To move scrapped products
-        @param self: The object pointer.
-        @param cr: A database cursor
-        @param uid: ID of the user currently logged in
-        @param ids: the ID or list of IDs if we want more than one
-        @param context: A standard dictionary
-        @return:
-        """
-        if context is None:
-            context = {}
-        move_obj = self.pool.get('stock.move')
-        move_ids = context['active_ids']
-        for data in self.browse(cr, uid, ids):
-            move_obj.action_scrap(cr, uid, move_ids,
-                             data.product_qty, data.location_id.id, restrict_lot_id=data.restrict_lot_id.id,
-                             context=context)
-        if context.get('active_id'):
-            move = self.pool.get('stock.move').browse(cr, uid, context['active_id'], context=context)
-            if move.picking_id:
-                return {
-                    'view_type': 'form',
-                    'view_mode': 'form',
-                    'res_model': 'stock.picking',
-                    'type': 'ir.actions.act_window',
-                    'res_id': move.picking_id.id,
-                    'context': context
-                }
-        return {'type': 'ir.actions.act_window_close'}
+    @api.onchange('picking_id', 'location_id', 'type')
+    def onchange_type(self):
+        if self.picking_id and self.type == 'reserved':
+            quants = self.env['stock.quant']
+            for move in self.picking_id.move_lines:
+                if self.picking_id.state == 'done':
+                    quants |= move.quant_ids
+                else:
+                    quants |= move.reserved_quant_ids
+            scrap_lines = self._translate_quants_to_lines(quants)
+            self.line_ids = scrap_lines
+
+    @api.multi
+    def do_scrap(self):
+        self.ensure_one()
+        # Create stock move in picking to put these quants to scrap
+        return True
+
+
+class StockPickingScrapLine(models.TransientModel):
+    _name = 'stock.scrap.line'
+
+    scrap_id = fields.Many2one('stock.scrap', 'Wizard', readonly=True)
+    location_id = fields.Many2one('stock.location', 'Location', readonly=True)
+    product_id = fields.Many2one('product.product', 'Product', readonly=True)
+    package_id = fields.Many2one('stock.quant.package', 'Package', readonly=True)
+    lot_id = fields.Many2one('stock.production.lot', 'Lot', readonly=True)
+    owner_id = fields.Many2one('res.partner', 'Owner', readonly=True)
+    product_qty = fields.Float('Qty In Stock', readonly=True)
+    scrap_qty = fields.Float('Qty To Scrap')
+    
