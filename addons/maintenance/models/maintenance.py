09149f42f6ab5fa77a0432af37ba600d95850fd5
cod-odoo <cod@openerp.com>
2015-12-15 18:51:20 +0530

    [IMP] maintenance: set default value for maintenance_type in request.

--- addons/maintenance/models/maintenance.py
+++ addons/maintenance/models/maintenance.py
@@ -235,7 +235,7 @@ class MaintenanceRequest(models.Model):
     kanban_state = fields.Selection([('normal', 'In Progress'), ('blocked', 'Blocked'), ('done', 'Ready for next stage')],
                                     string='Kanban State', required=True, default='normal', track_visibility='onchange')
     archive = fields.Boolean(default=False, help="Set archive to true to hide the maintenance request without deleting it.")
-    maintenance_type = fields.Selection([('corrective', 'corrective'), ('preventive', 'preventive')], string='Maintenance Type')
+    maintenance_type = fields.Selection([('corrective', 'corrective'), ('preventive', 'preventive')], string='Maintenance Type', default="corrective")
     maintenance_team_id = fields.Many2one('maintenance.team', string='Maintenance Team')
     duration = fields.Float(help="Duration in minutes and seconds.")
 
