ec8cece7a0a4e826d3bbcb1e1664eb3fe627f6b7
cod-odoo <cod@openerp.com>
2016-01-06 16:31:35 +0530

    [IMP]Did the stuff for scrap product qty.

--- addons/stock/stock_scrap.py
+++ addons/stock/stock_scrap.py
@@ -12,32 +12,31 @@ class StockScrap(models.Model):
     product_uom_id = fields.Many2one('product.uom', string='Product UoM')
     lot_id = fields.Many2one('stock.production.lot', 'Lot')
     picking_id = fields.Many2one('stock.picking', 'Picking')
-    location_id = fields.Many2one('stock.location', 'Source Location')
+    location_id = fields.Many2one('stock.location', 'Source Location', default=lambda self: self.env.ref('stock.warehouse0').lot_stock_id.id or False,)
     scrap_location_id = fields.Many2one('stock.location', domain="[('scrap_location', '=', True)]", string="Scrap Location", default=(lambda x: x.env['stock.location'].search([('scrap_location', '=', True)], limit=1)))
     scrap_qty = fields.Float('Qty To Scrap')
     state = fields.Selection([('confirmed', 'Confirmed'), ('done', 'Done')], default="confirmed")
+    move_id = fields.Many2one('stock.move', 'Stock Move')
 
-    def _translate_quants_to_lines(self, quants):
-        grouped_quants = {}
-        for quant in quants:
-            key = (quant.location_id.id, quant.product_id.id, quant.package_id.id, quant.lot_id.id, quant.owner_id.id)
-            if grouped_quants.get(key):
-                grouped_quants[key] += quant.qty
-            else:
-                grouped_quants[key] = quant.qty
-        scrap_lines = []
-        for key in grouped_quants:
-            scrap_lines += [{
-                'location_id': key[0],
-                'product_id': key[1],
-                'package_id': key[2],
-                'lot_id': key[3],
-                'owner_id': key[4],
-                'product_qty': grouped_quants[key]
-            }]
-            return scrap_lines
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        if self.product_id:
+            self.product_uom_id = self.product_id.uom_id.id
 
     @api.multi
     def do_scrap(self):
         self.ensure_one()
+        StockMove = self.env['stock.move']
+        default_val = {
+            'name': self.name,
+            'product_id': self.product_id.id,
+            'product_uom': self.product_uom_id.id,
+            'product_uom_qty': self.scrap_qty,
+            'location_id': self.location_id.id,
+            'scrapped': True,
+            'location_dest_id': self.scrap_location_id.id,
+        }
+        move = StockMove.create(default_val)
+        new_move = move.action_scrap(self.scrap_qty, self.scrap_location_id.id)
+        self.write({'move_id': new_move.id, 'state': 'done'})
         return True
