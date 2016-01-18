16dcb3854ba0c6df964334524bec00858c07f433
Gaurav Panchal <gan@openerp.com>
2016-01-18 18:11:46 +0530

    [IMP] maintenance : Improve form view of maintenance request and add some link on team kanban and remove alias from settings

--- addons/maintenance/__openerp__.py
+++ addons/maintenance/__openerp__.py
@@ -13,7 +13,6 @@
         'security/equipment.xml',
         'security/ir.model.access.csv',
         'data/equipment_data.xml',
-        'views/res_config_views.xml',
         'views/equipment_views.xml',
         'views/maintenance_dashboard.xml',
     ],
--- addons/maintenance/models/__init__.py
+++ addons/maintenance/models/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
 import maintenance
-import res_config
