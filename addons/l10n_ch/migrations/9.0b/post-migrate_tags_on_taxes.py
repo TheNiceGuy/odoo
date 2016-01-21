7100f1c8ddcf147b015fdb32ff8d673aa46bd1b3
qdp-odoo <qdp@odoo.com>
2016-01-21 16:53:33 +0100

    [REM] dubg statement

--- addons/l10n_ch/migrations/9.0b/post-migrate_tags_on_taxes.py
+++ addons/l10n_ch/migrations/9.0b/post-migrate_tags_on_taxes.py
@@ -1,8 +1,6 @@
 from openerp.modules.registry import RegistryManager
 
 def migrate(cr, version):
-    print '***' * 10
-    print 'dans le post_update'
     registry = RegistryManager.get(cr.dbname)
     from openerp.addons.account.models.chart_template import migrate_tags_on_taxes
     migrate_tags_on_taxes(cr, registry)
