6d4cf47740db6f32cb7c48b7b4d37ee730f5cbc8
Gaurav Panchal <gan@openerp.com>
2016-02-04 19:16:08 +0530

    [IMP] Add state button on kanban view.

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -196,7 +196,7 @@ class MrpProduction(models.Model):
     product_tmpl_id = fields.Many2one('product.template', related='product_id.product_tmpl_id', string='Product')
     categ_id = fields.Many2one('product.category', related='product_tmpl_id.categ_id', string='Product Category', readonly=True, store=True)
     check_to_done = fields.Boolean(compute="_check_to_done", string="Check Produced Qty")
-    warning_message = fields.Char(default='Raw material not available!', readonly=True)
+    warning_message = fields.Char(default='Raw materials not available!', readonly=True)
 
     _sql_constraints = [
         ('name_uniq', 'unique(name, company_id)', 'Reference must be unique per Company!'),
--- addons/mrp/models/mrp_workcenter.py
+++ addons/mrp/models/mrp_workcenter.py
@@ -23,13 +23,6 @@ class MrpWorkcenter(models.Model):
             workcenter.count_ready_order = WorkcenterLine.search_count([('workcenter_id', '=', workcenter.id), ('state', '=', 'ready')])
             workcenter.count_progress_order = WorkcenterLine.search_count([('workcenter_id', '=', workcenter.id), ('state', '=', 'progress')])
 
-    def _compute_status(self):
-        for workcenter in self:
-            if workcenter.count_progress_order:
-                workcenter.status = 'done'
-            else:
-                workcenter.status = 'normal'
-
     note = fields.Text(string='Description', help="Description of the Work Center. ")
     capacity = fields.Float(string='Capacity', default=1.0, help="Number of work orders this Work Center can do in parallel. If this Work Center represents a team of 5 workers, the capacity is 5.")
     time_start = fields.Float(string='Time before prod.', help="Time in hours for the setup.")
@@ -42,8 +35,8 @@ class MrpWorkcenter(models.Model):
     color = fields.Integer('Color')
     count_ready_order = fields.Integer(compute='_compute_orders', string="Total Ready Orders")
     count_progress_order = fields.Integer(compute='_compute_orders', string="Total Running Orders")
-    status = fields.Selection([('normal', 'Work order is not running'), ('blocked', 'Work center is block'), ('done', 'Work order is running')],
-                                    string='Status', compute="_compute_status")
+    stage_id = fields.Selection([('normal', 'Work order is not running'), ('blocked', 'Work center is blocked'), ('done', 'Work order is running')], string='Stage', default='normal')
+    kanban_state = fields.Selection([('normal', 'Work order is not running'), ('blocked', 'Work center is blocked'), ('done', 'Work order is running')], string='Status', default='normal')
 
     @api.multi
     @api.constrains('capacity')
@@ -65,4 +58,3 @@ class MrpWorkOrderConsume(models.Model):
     processed = fields.Boolean('Processed', default=False)
     final_lot_id = fields.Many2one('stock.production.lot', 'Final Lot')
     final_qty = fields.Float('Quantity')
-    
