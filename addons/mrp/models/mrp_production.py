13bb9051a4ef1efdf3032e6b31d15963fd68ade5
Josse Colpaert <jco@odoo.com>
2016-02-23 18:09:27 +0100

    [IMP] Algo for extra moves

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -278,13 +278,10 @@ class MrpProduction(models.Model):
     @api.multi
     def post_inventory(self):
         for order in self:
-            moves_to_do = order.move_raw_ids.filtered(lambda x: x.state not in ('done','cancel'))
-            moves_to_do.move_validate()
-            
+            moves_to_do = order.move_raw_ids.move_validate()
             #order.move_finished_ids.filtered(lambda x: x.state not in ('done','cancel')).move_validate()
-            moves_to_finish = order.move_finished_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
             order._cal_price(moves_to_do)
-            moves_to_finish.move_validate()
+            moves_to_finish = order.move_finished_ids.move_validate()
             for move in moves_to_finish:
                 quants = self.env['stock.quant']
                 #Group quants by lots
