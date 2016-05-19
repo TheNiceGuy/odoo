d0ccb40e402612b0a9cc5d93d994b8a89b735d12
Gaurav Panchal <gan@openerp.com>
2016-05-19 14:03:43 +0530

    [REM] mrp : Remove time counter widget (moved in enterprise)

--- addons/mrp/static/src/js/mrp.js
+++ addons/mrp/static/src/js/mrp.js
@@ -1,9 +1,8 @@
 odoo.define('mrp.mrp_state', function (require) {
+"use strict";
 
 var core = require('web.core');
 var common = require('web.form_common');
-var Model = require('web.Model');
-var time = require('web.time');
 
 var _t = core._t;
 
@@ -28,46 +27,5 @@ var SetBulletStatus = common.AbstractField.extend(common.ReinitializeFieldMixin,
     },
 });
 
-var TimeCounter = common.AbstractField.extend(common.ReinitializeWidgetMixin, common.ReinitializeFieldMixin, {
-    start: function() {
-        this._super();
-        var self = this;
-        this.field_manager.on("view_content_has_changed", this, function () {
-            self.render_value();
-        });
-    },
-    render_value: function() {
-        this._super.apply(this, arguments);
-        var self = this;
-        var timer, domain = [['workorder_id', '=', this.field_manager.datarecord.id], ['user_id', '=', self.session.uid]];
-        new Model('mrp.workcenter.productivity').call('search_read', [domain, []]).then(function(result) {
-            if (self.get("effective_readonly")) {
-                self.$el.removeClass('o_form_field_empty');
-                var current_date = new Date(), duration = 0;
-                _.each(result, function(data) {
-                    duration += data.date_end ? self.get_date_difference(data.date_start, data.date_end) : self.get_date_difference(time.auto_str_to_date(data.date_start), current_date);
-                });
-                function time_counter() {
-                    if (self.field_manager.datarecord.show_state) {
-                        duration += 1000;
-                        timer = setTimeout(function() {
-                            time_counter();
-                        }, 1000);
-                    } else {
-                        clearTimeout(timer);
-                    }
-                    self.$el.html($('<span>' + moment.utc(duration).format("HH:mm:ss") + '</span>'));
-                }
-                time_counter();
-            }
-        });
-    },
-    get_date_difference: function(date_start, date_end) {
-        var difference = moment(date_end).diff(moment(date_start));
-        return moment.duration(difference);
-    },
-});
-
-core.form_widget_registry.add('bullet_state', SetBulletStatus)
-                         .add('mrp_time_counter', TimeCounter);
+core.form_widget_registry.add('bullet_state', SetBulletStatus);
 });
