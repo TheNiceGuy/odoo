ce395e189da34552dc67abc7f8c8f8c78cdb4aee
Jérome Maes <jem@openerp.com>
2016-05-30 15:40:27 +0200

    [MIG] website_crm_partner_assign : migration ot new api

--- addons/website_crm_partner_assign/models/res_partner.py
+++ addons/website_crm_partner_assign/models/res_partner.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import osv
-from openerp.addons.website.models.website import slug
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+from odoo.addons.website.models.website import slug
 
 
-class res_partner_grade(osv.osv):
+class Grade(models.Model):
+
     _name = 'res.partner.grade'
     _inherit = ['res.partner.grade', 'website.published.mixin']
 
-    _defaults = {
-        'website_published': True,
-    }
+    website_published = fields.Boolean(default=True)
 
-    def _website_url(self, cr, uid, ids, field_name, arg, context=None):
-        res = super(res_partner_grade, self)._website_url(cr, uid, ids, field_name, arg, context=context)
-        for grade in self.browse(cr, uid, ids, context=context):
-            res[grade.id] = "/partners/grade/%s" % (slug(grade))
+    @api.multi
+    def _website_url(self, field_name, arg):
+        res = super(Grade, self)._website_url(field_name, arg)
+        for grade in self:
+            res[grade.id] = "/partners/grade/%s" % slug(grade)
         return res
