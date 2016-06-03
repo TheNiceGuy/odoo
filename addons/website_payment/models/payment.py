fd789a8a4d8d0b6d7a70edea2818e5586d5d6b52
keyur gajjar <kga@odoo.com>
2016-06-03 11:15:42 +0530

    [ADD]website_payment: boolean field needed to handle the ups cod type payments

--- addons/website_payment/models/payment.py
+++ addons/website_payment/models/payment.py
@@ -1,6 +1,8 @@
-from openerp import models
+from openerp import fields, models
 
 
 class PaymentAcquirer(models.Model):
     _name = 'payment.acquirer'
     _inherit = ['payment.acquirer','website.published.mixin']
+
+    is_cod = fields.Boolean()
