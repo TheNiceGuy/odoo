d16e5c992d7f734c08ce20a2dd42086246aab418
Simon Lejeune <sle@openerp.com>
2016-06-17 17:49:54 +0200

    m

--- addons/report/static/src/js/client_action.js
+++ addons/report/static/src/js/client_action.js
@@ -17,15 +17,19 @@ var ReportAction = Widget.extend(ControlPanelMixin, {
         this.options = options || {};
         this.action_manager = parent;
 
+        this.in_edit_mode = false;
+
         core.bus.on('message', this, this.on_message_received);
     },
 
     start: function () {
         var self = this;
         return this._super.apply(this, arguments).then(function () {
-            self.$buttons = $(QWeb.render("report.client_action.ControlButtons", {}));
+            self.$buttons = $(QWeb.render('report.client_action.ControlButtons', {}));
             self.$buttons.on('click', '.o_report_edit', self.on_click_edit);
             self.$buttons.on('click', '.o_report_print', self.on_click_print);
+            self.$buttons.on('click', '.o_report_save', self.on_click_save);
+            self.$buttons.on('click', '.o_report_discard', self.on_click_discard);
 
             self.$el.css({
                 position: 'absolute',
@@ -33,9 +37,9 @@ var ReportAction = Widget.extend(ControlPanelMixin, {
                 right: 0,
                 bottom: 0,
                 left: 0,
-                width: "100%",
-                height: "100%",
-                border: "none",
+                width: '100%',
+                height: '100%',
+                border: 'none',
             });
 
             self.el.src = self.action.report_url;
@@ -46,15 +50,28 @@ var ReportAction = Widget.extend(ControlPanelMixin, {
                     $buttons: self.$buttons,
                 },
             });
-        })
+            self._update_control_panel_buttons();
+        });
+    },
+
+    _update_control_panel_buttons: function () {
+        this.$buttons.filter('div.o_report_edit_mode').toggle(this.in_edit_mode);
+        this.$buttons.filter('div.o_report_no_edit_mode').toggle(! this.in_edit_mode);
     },
 
     on_message_received: function (ev) {
         if (ev.originalEvent.source.location.host === window.location.host &&
                 ev.originalEvent.source.location.protocol === window.location.protocol) {
             switch(ev.originalEvent.data) {
-                case 'do_action':
-                    this.do_action(ev.originalEvent.data);
+                case 'report.editor:save_ok':
+                    this.el.src = this.action.report_url;  // reload to disable the editor
+                    this.in_edit_mode = false;
+                    this._update_control_panel_buttons();
+                    break;
+                case 'report.editor:discard_ok':
+                    this.el.src = this.action.report_url; // reload to disable the editor
+                    this.in_edit_mode = false;
+                    this._update_control_panel_buttons();
                     break;
                 default:
                     console.log('nothing to do for ' + ev.originalEvent.data);
@@ -64,14 +81,16 @@ var ReportAction = Widget.extend(ControlPanelMixin, {
 
     on_click_edit: function () {
         this.el.src = this.action.report_url + '?enable_editor=1';
+        this.in_edit_mode = true;
+        this._update_control_panel_buttons();
     },
 
     on_click_discard: function () {
-        this.el.src = this.action.report_url;
+        this.el.contentWindow.postMessage('report.editor:ask_discard', 'http://127.0.0.1:8069');
     },
 
     on_click_save: function () {
-        this.el.postMessage('web_editor_save');
+        this.el.contentWindow.postMessage('report.editor:ask_save', 'http://127.0.0.1:8069');
     },
 
     on_click_print: function () {
--- addons/report/static/src/js/report.editor.js
+++ addons/report/static/src/js/report.editor.js
@@ -4,10 +4,70 @@ odoo.define('report.editor', function (require) {
 var editor = require('web_editor.editor');
 var options = require('web_editor.snippets.options');
 
-var old_save = editor.save;
-editor.save = function () {
-    old_save.apply(this, arguments);
+var is_in_iframe = function () {
+    return window.self !== window.top;
+};
+
+var post_message = function (message) {
+    window.parent.postMessage(message, "http://127.0.0.1:8069");
+};
+
+var on_message_received = function (ev) {
+    if (ev.source.location.host === window.location.host &&
+            ev.source.location.protocol === window.location.protocol) {
+        switch(ev.data) {
+            case 'report.editor:ask_save':
+                console.log('assking save');
+                editor.editor_bar.save();
+                break;
+            case 'report.editor:ask_discard':
+                editor.editor_bar.cancel();
+                break;
+            default:
+                console.log('nothing to do for ' + ev.data);
+        }
+    }
+};
+
+if (is_in_iframe()) {
+    window.addEventListener('message', on_message_received, false);
+
+    // As editor.reload is called after `save` and `cancel`, we remove this
+    // function in the iframe in order to be able to chain deferred after the
+    // previously named method.
+    editor.reload = function () {
+        return $.when();
+    };
 }
+
+editor.Class.include({
+    save: function () {
+        // Cannot use `super.then()` as the default implementation triggers a
+        // reload of the page.
+        // debugger
+        var res = this.rte.save();  // FIXME why not _super?
+        if (is_in_iframe()) {
+            res.then(function () {
+                post_message('report.editor:save_ok');
+            }, function () {
+                post_message('report.editor:save_ko');
+            });
+        }
+        return res;
+    },
+
+    cancel: function () {
+        var res = this._super.apply(this, arguments);
+        if (is_in_iframe()) {
+            res.then(function () {
+                post_message('report.editor:discard_ok');
+            }, function () {
+                post_message('report.editor:dicard_ko');
+            });
+        }
+        return res;
+    },
+});
 
 options.registry.many2one.include({
     select_record: function (li) {
@@ -22,7 +82,5 @@ options.registry.many2one.include({
         }
     }
 });
-
-window.parent.postMessage("teseeeet", "http://127.0.0.1:8069");
 
 });
