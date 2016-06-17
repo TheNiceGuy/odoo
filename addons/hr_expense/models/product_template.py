f0763cc0795f3017799850dc5b6f7725c13ea98b
Mansi Gandhi <mag@odoo.com>
2016-06-17 12:08:13 +0530

    [IMP] hr_expense: Remove expenses type products from Point of sale app.

--- addons/hr_expense/models/product_template.py
+++ addons/hr_expense/models/product_template.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -9,3 +9,9 @@ class ProductTemplate(models.Model):
 
     can_be_expensed = fields.Boolean(help="Specify whether the product can be selected in an HR expense.", string="Can be Expensed")
 
+     
+    @api.onchange('can_be_expensed')
+    def onchange_can_be_expensed(self):
+        if hasattr(self, 'available_in_pos'):
+            if self.can_be_expensed:
+                self.available_in_pos = False
